09c40370d54f646af79463deadfafaa8edd28f83
Marius Aalto <marius.aalto@outlook.com>
2024-02-23 10:21:07 +0100

    Update model.js

---
+++ model.js
@@ -0,0 +1,41 @@
+const model = {
+  app: {
+    currentPage: "home",
+  },
+  input: {
+    searchBar: "",
+    chosenTootId: 3,
+    newToot: {
+      name: "",
+      year: "",
+      type: "",
+      value: "",
+      producer: "",
+    },
+  },
+  data: {
+    myToots: [
+      {
+        name: "BertBerten",
+        year: 1997,
+        type: "Aalto recorder",
+        value: "231",
+        producer: "Aalto Industries AS",
+      },
+      {
+        name: "Tut Tutesen",
+        year: 2005,
+        type: "Sopran",
+        value: "419",
+        producer: "Yamaha",
+      },
+      {
+        name: "IRF21",
+        year: 2009,
+        type: "fife",
+        value: "900",
+        producer: "Yamaha",
+      },
+    ],
+  },
+};
